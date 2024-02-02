08cc5277847f9974f30e28c87b8dc6fbfbb93968
zhangxiang <501696487@qq.com>
2024-02-02 22:18:07 +0800

    修改config.js2

--- deploy.sh
+++ deploy.sh
@@ -3,19 +3,18 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-echo $GITHUB_TOKEN
 #push_addr=`git remote get-url --push origin` # git提交地址，也可以手动设置，比如：push_addr=git@github.com:xugaoyi/vuepress-theme-vdoing.git
-if [ -z "$GITHUB_TOKEN" ]; then
-    push_addr=`git remote get-url --push origin`
-    # 解决node报错
-    export NODE_OPTIONS=--openssl-legacy-provider 
-else
-    push_addr=`git remote get-url --push origin`
-    push_addr=`echo $push_addr | awk -F'://' '{print $2}'`
-    push_addr=https://zhangxiang:${GITHUB_TOKEN}@${push_addr}
-    git config user.email "501696487@qq.com"
-    git config user.name "zhangxiang"
-fi
+# if [ -z "$GITHUB_TOKEN" ]; then
+#     push_addr=`git remote get-url --push origin`
+#     # 解决node报错
+#     export NODE_OPTIONS=--openssl-legacy-provider 
+# else
+push_addr=`git remote get-url --push origin`
+push_addr=`echo $push_addr | awk -F'://' '{print $2}'`
+push_addr=https://zhangxiang:${GITHUB_TOKEN}@${push_addr}
+git config user.email "501696487@qq.com"
+git config user.name "zhangxiang"
+# fi
 commit_info=`git describe --all --always --long`
 dist_path=dist # 打包生成的文件夹路径
 push_branch=trunk # 推送的分支
