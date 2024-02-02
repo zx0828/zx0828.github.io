5d05efaafaca8ebf4b3956cb733c393b4e65d304
zhangxiang <501696487@qq.com>
2024-02-02 21:09:46 +0800

    添加输出环境变量11

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ set -e
 
 
 #push_addr=`git remote get-url --push origin` # git提交地址，也可以手动设置，比如：push_addr=git@github.com:xugaoyi/vuepress-theme-vdoing.git
-push_addr=`https://${GITHUB_TOKEN}@github.com/zhangxiang/zx0828.github.io.git`
+push_addr=`https://${GITHUB_TOKEN}@github.com/zx0828/zx0828.github.io.git`
 commit_info=`git describe --all --always --long`
 dist_path=dist # 打包生成的文件夹路径
 push_branch=trunk # 推送的分支
