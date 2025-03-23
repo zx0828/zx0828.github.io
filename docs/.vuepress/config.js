17954e86697eb186118188a1ae0c9777d9440b5a
zhangxiang <501696487@qq.com>
2025-03-23 16:22:25 +0800

    config.js 增加配置markdown,动增加前缀

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -19,88 +19,88 @@ module.exports = {
         editLinks: true, // 启用编辑
         editLinkText: '编辑',
         nav: [{
-                text: '首页',
-                link: '/'
+            text: '首页',
+            link: '/'
+        },
+        {
+            text: '前端',
+            link: '/web/'
+        },
+        {
+            text: '后端',
+            link: '/serve/'
+        },
+        {
+            text: '框架',
+            link: '/technology/'
+        },
+        {
+            text: '云技术',
+            link: '/technology/'
+        },
+        {
+            text: '收藏',
+            link: '/pages/beb6c0bd8a66cea6/',
+            items: [{
+                text: '网站',
+                link: '/pages/beb6c0bd8a66cea6/'
             },
             {
-                text: '前端',
-                link: '/web/'
+                text: '资源',
+                link: '/pages/eee83a9211a70f9d/'
             },
             {
-                text: '后端',
-                link: '/serve/'
+                text: 'Vue资源',
+                link: '/pages/12df8ace52d493f6/'
+            },
+            ],
+        },
+        {
+            text: '更多',
+            link: '/more/',
+            items: [{
+                text: '学习',
+                link: '/pages/f2a556/'
             },
             {
-                text: '框架',
-                link: '/technology/'
+                text: '面试',
+                link: '/pages/aea6571b7a8bae86/'
             },
             {
-                text: '云技术',
-                link: '/technology/'
+                text: '心情杂货',
+                link: '/pages/2d615df9a36a98ed/'
             },
             {
-                text: '收藏',
-                link: '/pages/beb6c0bd8a66cea6/',
-                items: [{
-                        text: '网站',
-                        link: '/pages/beb6c0bd8a66cea6/'
-                    },
-                    {
-                        text: '资源',
-                        link: '/pages/eee83a9211a70f9d/'
-                    },
-                    {
-                        text: 'Vue资源',
-                        link: '/pages/12df8ace52d493f6/'
-                    },
-                ],
+                text: '实用技巧',
+                link: '/pages/baaa02/'
             },
             {
-                text: '更多',
-                link: '/more/',
-                items: [{
-                        text: '学习',
-                        link: '/pages/f2a556/'
-                    },
-                    {
-                        text: '面试',
-                        link: '/pages/aea6571b7a8bae86/'
-                    },
-                    {
-                        text: '心情杂货',
-                        link: '/pages/2d615df9a36a98ed/'
-                    },
-                    {
-                        text: '实用技巧',
-                        link: '/pages/baaa02/'
-                    },
-                    {
-                        text: '友情链接',
-                        link: '/friends/'
-                    },
-                ],
+                text: '友情链接',
+                link: '/friends/'
+            },
+            ],
+        },
+        {
+            text: '其他',
+            link: '/other/'
+        },
+        {
+            text: '索引',
+            link: '/archives/',
+            items: [{
+                text: '分类',
+                link: '/categories/'
             },
             {
-                text: '其他',
-                link: '/other/'
+                text: '标签',
+                link: '/tags/'
             },
             {
-                text: '索引',
-                link: '/archives/',
-                items: [{
-                        text: '分类',
-                        link: '/categories/'
-                    },
-                    {
-                        text: '标签',
-                        link: '/tags/'
-                    },
-                    {
-                        text: '归档',
-                        link: '/archives/'
-                    },
-                ]
-            }
+                text: '归档',
+                link: '/archives/'
+            },
+            ]
+        }
         ],
         bodyBgImg: [
             `/zx_docs/img/a.jpeg`,
@@ -109,6 +109,11 @@ module.exports = {
             `/zx_docs/img/d.jpeg`
         ], // body背景大图，默认无。 单张图片 String || 多张图片 Array, 多张图片时每隔15秒换一张。
         bodyBgImgOpacity: 1, // body背景图透明度，选值 0 ~ 1.0, 默认0.5
+    },
+    markdown: {
+        assets: {
+            absolutePathPrependBase: true, // 自动为 Markdown 图片添加 base 前缀
+        },
     },
     plugins
 }
